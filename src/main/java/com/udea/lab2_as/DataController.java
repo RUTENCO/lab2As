b65c5f0b250d0282a3923334c94556beec3e2cd5
RUTENCO <daniel.ramirez7@udea.edu.co>
2025-10-26 19:47:12 -0500

    fix issues from SonarQube

--- src/main/java/com/udea/lab2_as/DataController.java
+++ src/main/java/com/udea/lab2_as/DataController.java
@@ -13,7 +13,7 @@ import com.github.javafaker.Faker;
 
 @RestController
 public class DataController {
-    private final String language = "en-US";
+    private static final String LANGUAGE = "en-US";
     
     @GetMapping("/")
     public String getHealth() {
@@ -28,7 +28,7 @@ public class DataController {
     @GetMapping("/nations")
     public JsonNode getRandomNations() {
         var objectMapper = new ObjectMapper();
-        var faker = new Faker(new Locale(language));
+        var faker = new Faker(new Locale(LANGUAGE));
         var nations = objectMapper.createArrayNode();
         for (int i = 0; i < 10; i++) {
             var nation = faker.nation();
@@ -44,7 +44,7 @@ public class DataController {
     @GetMapping("/currencies")
     public JsonNode getRandomCurrencies() {
         var objectMapper = new ObjectMapper();
-        var faker = new Faker(new Locale(language));
+        var faker = new Faker(new Locale(LANGUAGE));
         var currencies = objectMapper.createArrayNode();
         for (int i = 0; i < 20; i++) {
             var currency = faker.currency();
@@ -58,7 +58,7 @@ public class DataController {
     @GetMapping("/aviations")
     public JsonNode getRandomAviations() {
         var objectMapper = new ObjectMapper();
-        var faker = new Faker(new Locale(language));
+        var faker = new Faker(new Locale(LANGUAGE));
         var aviations = objectMapper.createArrayNode();
         for (int i = 0; i < 20; i++) {
             var aviation = faker.aviation();
